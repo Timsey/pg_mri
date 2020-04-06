f72da9061f84dd5a52a2116de5c3cf8b869c546f
Timsey <tim_bb@hotmail.com>
2020-04-06 17:24:11 +0200

    edited in QS 128 res bash file

--- das5_runfiles/das5_train_improQS_kengal80.sh
+++ das5_runfiles/das5_train_improQS_kengal80.sh
@@ -1,58 +1,17 @@
 #!/usr/bin/env bash
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convbottle --num-epochs 100 \
---fc-size 256 --out-chans 64 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 10 --lr 1e-3 --sample-rate 0.04 \
---seed 42 --eps-decay-rate 1 --num-workers 8 --in-chans 1 --lr-gamma 10 --center-volume --start-eps 0.5 --tau 0.1 \
---wandb --scheduler-type multistep --lr-multi-step-size 40 --acquisition CORPD_FBK
+--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
+--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
+--num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 512 --accelerations 8 --acquisition-steps 16 \
+--report-interval 10 --num-target-rows 112 --lr 1e-4 --sample-rate 0.04 --seed 42 --eps-decay-rate 5 --num-workers 8 --in-chans 1 --center-volume \
+--lr-gamma 0.1 --num-epochs 50 --scheduler-type step --lr-step-size 40 --start-eps 0 --start-tau 0.1 --tau-decay-rate 1 --do-dev-loss --pool-stride 1 \
+--do-train-ssim --verbose 1 --wandb
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convbottle --num-epochs 100 \
---fc-size 256 --out-chans 64 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 10 --lr 1e-3 --sample-rate 0.04 \
---seed 42 --eps-decay-rate 5 --num-workers 8 --in-chans 1 --lr-gamma 10 --center-volume --start-eps 0.5 --tau 0.1 \
---wandb --scheduler-type multistep --lr-multi-step-size 40 --acquisition CORPD_FBK
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convbottle --num-epochs 100 \
---fc-size 256 --out-chans 64 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 10 --lr 1e-3 --sample-rate 0.04 \
---seed 42 --eps-decay-rate 10 --num-workers 8 --in-chans 1 --lr-gamma 10 --center-volume --start-eps 0.5 --tau 0.1 \
---wandb --scheduler-type multistep --lr-multi-step-size 40 --acquisition CORPD_FBK
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --num-epochs 100 \
---fc-size 256 --out-chans 64 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 10 --lr 1e-3 --sample-rate 0.04 \
---seed 42 --eps-decay-rate 1 --num-workers 8 --in-chans 1 --lr-gamma 10 --center-volume --start-eps 0.5 --tau 0.1 \
---wandb --scheduler-type multistep --lr-multi-step-size 40 --acquisition CORPD_FBK
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --num-epochs 100 \
---fc-size 256 --out-chans 64 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 10 --lr 1e-3 --sample-rate 0.04 \
---seed 42 --eps-decay-rate 5 --num-workers 8 --in-chans 1 --lr-gamma 10 --center-volume --start-eps 0.5 --tau 0.1 \
---wandb --scheduler-type multistep --lr-multi-step-size 40 --acquisition CORPD_FBK
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --num-epochs 100 \
---fc-size 256 --out-chans 64 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 10 --lr 1e-3 --sample-rate 0.04 \
---seed 42 --eps-decay-rate 10 --num-workers 8 --in-chans 1 --lr-gamma 10 --center-volume --start-eps 0.5 --tau 0.1 \
---wandb --scheduler-type multistep --lr-multi-step-size 40 --acquisition CORPD_FBK
-
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQS_model \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_gauss_res80_8to4in2_PD_cvol/model.pt --recon-model-name kengal_gauss \
---resolution 80 --num-pools 8 --of-which-four-pools 0 --num-chans 12 --batch-size 16 --impro-model-name convpool --num-epochs 100 \
---fc-size 2048 --accelerations 8 --acquisition-steps 10 --report-interval 10 --num-target-rows 70 --lr 2e-5 --sample-rate 0.04 \
---seed 42 --num-workers 8 --in-chans 1 --lr-gamma 0.1 --center-volume --start-eps 0.0 --tau 0.1 --tau-decay-rate 1 \
---wandb --scheduler-type step --lr-step-size 80 --acquisition CORPD_FBK --do-dev-loss --pool-stride 2 --do-train-ssim --verbose 0
+--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
+--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
+--num-pools 4 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 \
+--report-interval 10 --num-target-rows 112 --lr 1e-4 --sample-rate 0.04 --seed 42 --eps-decay-rate 5 --num-workers 8 --in-chans 1 --center-volume \
+--lr-gamma 0.1 --num-epochs 50 --scheduler-type step --lr-step-size 40 --start-eps 0 --start-tau 0.1 --tau-decay-rate 1 --do-dev-loss --pool-stride 1 \
+--do-train-ssim --verbose 1 --wandb
