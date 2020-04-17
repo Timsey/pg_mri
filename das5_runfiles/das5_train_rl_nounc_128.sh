54220ff2df6c96824881a58e7be49ca99e24f62a
Timsey <tim_bb@hotmail.com>
2020-04-17 13:20:26 +0200

    more das5

--- das5_runfiles/das5_train_rl_nounc_128.sh
+++ das5_runfiles/das5_train_rl_nounc_128.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
 
+## REINFORCE with step baseline option 1)
+#CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
+#--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
+#--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
+#--of-which-four-pools 0 --num-chans 20 --batch-size 8 --impro-model-name convpool --fc-size 1024 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+#--lr 5e-5 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
+#--estimator start_adaptive --num-trajectories 4 --num-dev-trajectories 4 --greedy False
+#
+## REINFORCE with step baseline option 3)
+## 24 chans, 1024 fc, depth 4, traj 4 is doable on node206 (traj 8 gives memory error)
+#CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
+#--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
+#--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
+#--of-which-four-pools 0 --num-chans 20 --batch-size 8 --impro-model-name convpool --fc-size 1024 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+#--lr 5e-5 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
+#--estimator full_step --num-trajectories 4 --num-dev-trajectories 4 --greedy False
+
 # REINFORCE with step baseline option 1)
 CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
 --dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
 --recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 20 --batch-size 8 --impro-model-name convpool --fc-size 1024 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
---lr 5e-5 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
---estimator start_adaptive --num-trajectories 4 --num-dev-trajectories 4 --greedy False
+--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+--lr 1e-4 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
+--estimator start_adaptive --num-trajectories 8 --num-dev-trajectories 8 --greedy False
 
 # REINFORCE with step baseline option 3)
-# 24 chans, 1024 fc, depth 4, traj 4 is doable on node206 (traj 8 gives memory error)
 CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
 --dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
 --recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 20 --batch-size 8 --impro-model-name convpool --fc-size 1024 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
---lr 5e-5 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
---estimator full_step --num-trajectories 4 --num-dev-trajectories 4 --greedy False
+--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+--lr 1e-4 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
+--estimator full_step --num-trajectories 8 --num-dev-trajectories 8 --greedy False
+
+# REINFORCE with step baseline greedy
+CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
+--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
+--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_PD_cvol_ch16_b64_symk/model.pt --recon-model-name nounc \
+--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+--lr 1e-4 --sample-rate 0.04 --seed 42 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
+--estimator full_step --num-trajectories 8 --num-dev-trajectories 8 --greedy True
 
