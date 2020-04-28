7267e2892e09a7c41a9d58ab9be246a547ab214e
Timsey <tim_bb@hotmail.com>
2020-04-28 13:35:10 +0200

    more das5 experiments

--- src/train_RL_model_sweep.py
+++ src/train_RL_model_sweep.py
@@ -871,7 +871,7 @@ if __name__ == '__main__':
 
     args.use_recon_mask_params = False
 
-    args.wandb = False
+    args.wandb = True
     if args.wandb:
         wandb.init(project='mrimpro', config=args)
 
