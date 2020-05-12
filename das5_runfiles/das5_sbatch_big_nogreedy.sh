ec20b6a640de266fe08d80193efa2b0db1227a6b
Timsey <tim_bb@hotmail.com>
2020-05-12 13:08:11 +0200

    das5

--- das5_runfiles/das5_sbatch_big_nogreedy.sh
+++ das5_runfiles/das5_sbatch_big_nogreedy.sh
@@ -9,7 +9,7 @@ echo "Starting"
 source /var/scratch/tbbakker/anaconda3/bin/activate fastmri
 nvidia-smi
 
-# On full data
+# On half data (need data-state!)
     # 16-32
         # Both acquisitions
             # Nongreedy with self baseline (schedule)
@@ -20,4 +20,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fas
 --of-which-four-pools 0 --num-chans 16 --batch-size 8 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
 --lr 1e-4 --sample-rate 0.5 --seed 0 --num-workers 0 --in-chans 1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
 --estimator full_step --num-trajectories 8 --num-dev-trajectories 4 --greedy False --data-range volume --baseline-type selfstep \
---scheduler-type multistep --lr-multi-step-size 10 20 30 40 --lr-gamma .5 --acquisition None --center-volume True --batches-step 1
+--scheduler-type multistep --lr-multi-step-size 10 20 30 40 --lr-gamma .5 --acquisition None --center-volume True --batches-step 1  --use-data-state True
