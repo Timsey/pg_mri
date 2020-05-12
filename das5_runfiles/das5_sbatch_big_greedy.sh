7ba857e6fbea44460bcf4acdb9d443f170bcfb43
Timsey <tim_bb@hotmail.com>
2020-05-12 12:01:41 +0200

    das5

--- das5_runfiles/das5_sbatch_big_greedy.sh
+++ das5_runfiles/das5_sbatch_big_greedy.sh
@@ -18,15 +18,15 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fas
 --dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
 --recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
---num-target-rows 20 --lr 1e-4 --sample-rate 1 --seed 0 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
+--num-target-rows 20 --lr 1e-4 --sample-rate 0.5 --seed 0 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --lr-step-size 40 --num-pools 4 --pool-stride 1 \
 --estimator wr --acq_strat max --acquisition None --center-volume True
                 # 30 epochs
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQR_model_sweep \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
---num-target-rows 20 --lr 1e-4 --sample-rate 1 --seed 0 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 30 --lr-step-size 20 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat max --acquisition None --center-volume True
+#CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQR_model_sweep \
+#--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/results/ --resolution 128 \
+#--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
+#--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+#--num-target-rows 20 --lr 1e-4 --sample-rate 1 --seed 0 --num-workers 0 --in-chans 1 --lr-gamma 0.1 --num-epochs 30 --lr-step-size 20 --num-pools 4 --pool-stride 1 \
+#--estimator wr --acq_strat max --acquisition None --center-volume True
 #            # Nongreedy with self baseline (schedule)
 #                # 50 epochs
 #CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
