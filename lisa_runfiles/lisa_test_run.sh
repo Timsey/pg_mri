7e83795f143580351adfff5b11697c4a00c0c296
Timsey <tim_bb@hotmail.com>
2020-05-19 13:08:40 +0200

    lisa

--- lisa_runfiles/lisa_test_run.sh
+++ lisa_runfiles/lisa_test_run.sh
@@ -23,7 +23,7 @@ nvidia-smi
 mkdir "$TMPDIR"/data
 
 #Copy input file to scratch
-cp $HOME/data/singlecoil "$TMPDIR"/data
+cp $HOME/data/fastMRI/singlecoil "$TMPDIR"/data
 
 #Create output directory on scratch
 mkdir "$TMPDIR"/results
@@ -31,13 +31,10 @@ mkdir "$TMPDIR"/results
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path "$TMPDIR"/data/singlecoil/ --exp-dir "$TMPDIR"/results/ --resolution 128 \
 --recon-model-checkpoint /home/tbbakker/Projects/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
+--of-which-four-pools 0 --num-chans 8 --batch-size 64 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 2 --report-interval 100 \
 --num-target-rows 2 --lr 1e-4 --sample-rate 0.04 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 1 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat max --acquisition None --center-volume True --scheduler-type multistep --lr-multi-step-size 10 20 \
---wandb True --do-train-ssim False
+--estimator wr --acq_strat sample --acquisition None --center-volume True \
+--wandb False --do-train-ssim False
 
 #Copy output directory from scratch to home
 cp -r "$TMPDIR"/results $HOME/Projects/mrimpro/results
-
-
-ERROR: FileNotFoundError: [Errno 2] No such file or directory: '/scratch/data/singlecoil/singlecoil_train_al' in create_data_loaders.
