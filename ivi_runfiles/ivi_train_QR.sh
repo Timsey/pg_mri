b82969e2a0d060e8d884871eaaf479fa1f041df3
Timsey <tim_bb@hotmail.com>
2020-05-23 14:05:23 +0200

    resume ivi

--- ivi_runfiles/ivi_train_QR.sh
+++ ivi_runfiles/ivi_train_QR.sh
@@ -20,18 +20,18 @@ nvidia-smi
 # Do your stuff
 
 # 1033
-CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
---data-path /home/tbbakke/data/fastMRI/singlecoil/ \
---recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
---wandb True \
---resume True --run_id 16f367ps --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k16_2020-05-21_01:18:40/model.pt
-
-# 1046
 #CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 #--data-path /home/tbbakke/data/fastMRI/singlecoil/ \
 #--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
 #--wandb True \
-#--resume True --run_id x8f8areg --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k8_2020-05-22_13:10:25/model.pt
+#--resume True --run_id 16f367ps --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k16_2020-05-21_01:18:40/model.pt
+
+# 1046
+CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
+--data-path /home/tbbakke/data/fastMRI/singlecoil/ \
+--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
+--wandb True \
+--resume True --run_id x8f8areg --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k8_2020-05-22_13:10:25/model.pt
 
 # 1048
 #CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
