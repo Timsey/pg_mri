5c4ccf9a91fea7375d485030c158e2d26280949f
Timsey <tim_bb@hotmail.com>
2020-05-23 13:13:12 +0200

    resume ivi

--- ivi_runfiles/ivi_train_QR.sh
+++ ivi_runfiles/ivi_train_QR.sh
@@ -31,7 +31,7 @@ nvidia-smi
 
 # 1046
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
---wandb True\
+--wandb True \
 --resume True --run_id x8f8areg --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k8_2020-05-22_13:10:25/model.pt
 
 # 1048
