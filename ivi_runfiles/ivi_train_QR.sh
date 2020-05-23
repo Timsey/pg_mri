11627f71b361abbef74d584e0ac831b0cb536f1c
Timsey <tim_bb@hotmail.com>
2020-05-23 13:20:38 +0200

    resume ivi

--- ivi_runfiles/ivi_train_QR.sh
+++ ivi_runfiles/ivi_train_QR.sh
@@ -31,10 +31,14 @@ nvidia-smi
 
 # 1046
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
+--data-path /home/tbbakke/data/fastMRI/singlecoil/ \
+--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
 --wandb True \
 --resume True --run_id x8f8areg --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k8_2020-05-22_13:10:25/model.pt
 
 # 1048
 #CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
+#--data-path /home/tbbakke/data/fastMRI/singlecoil/ \
+#--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
 #--wandb True  \
 #--resume True --run_id 2bgdrs2f --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k8_2020-05-22_13:10:53/model.pt
