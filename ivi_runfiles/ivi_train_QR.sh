77dd74982d04558e291d4bf35b54f1f1d75e58e4
Timsey <tim_bb@hotmail.com>
2020-05-26 15:52:07 +0200

    ivi

--- ivi_runfiles/ivi_train_QR.sh
+++ ivi_runfiles/ivi_train_QR.sh
@@ -19,12 +19,19 @@ nvidia-smi
 
 # Do your stuff
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
---data-path /home/tbbakke/data/fastMRI/singlecoil/ --exp-dir /home/tbbakke/mrimpro/exp_results/ --resolution 128 \
---recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
---num-target-rows 8 --lr 1e-4 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 \
---wandb True --do-train-ssim True --num-test-trajectories 4
+--data-path /home/tbbakke/data/fastMRI/singlecoil/ \
+--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
+--wandb True \
+--resume True --run_id m8f1k0ij --impro-model-checkpoint /home/tbbakke/mrimpro/exp_results/res128_al28_accel[32]_convpool_nounc_k8_2020-05-25_23:00:34/model.pt
+
+
+#CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
+#--data-path /home/tbbakke/data/fastMRI/singlecoil/ --exp-dir /home/tbbakke/mrimpro/exp_results/ --resolution 128 \
+#--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
+#--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
+#--num-target-rows 8 --lr 1e-4 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
+#--estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 \
+#--wandb True --do-train-ssim True --num-test-trajectories 4
 
 
 # 1033
