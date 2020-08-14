79c301b45cd75fdde6f60345bb33a360a1254ea9
Timsey <tim_bb@hotmail.com>
2020-08-14 12:57:33 +0200

    das5 run

---
+++ das5_runfiles/das5_sbatch_gamma.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#SBATCH --job-name=nogreedy
+#SBATCH --gres=gpu:4  # Hoeveel gpu heb je nodig?
+#SBATCH -C GTX980Ti|GTX1080Ti|TitanX  # Welke gpus heb je nodig?
+
+echo "Starting"
+
+source /var/scratch/tbbakker/anaconda3/bin/activate fastmri
+nvidia-smi
+
