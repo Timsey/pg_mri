a89b4c9886eb369a117002284f7dac55363ee345
Timsey <tim_bb@hotmail.com>
2020-09-09 12:10:11 +0200

    lisa

--- lisa_runfiles/lisa_train_QR_16_32.sh
+++ lisa_runfiles/lisa_train_QR_16_32.sh
@@ -32,18 +32,10 @@ nvidia-smi
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path "$TMPDIR"/data/singlecoil/ --exp-dir "$TMPDIR"/results/ --resolution 128 \
 --recon-model-checkpoint /home/tbbakker/Projects/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
---num-target-rows 16 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --num-test-trajectories 4 \
---wandb True --do-train-ssim True
-
-#CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
-#--dataset fastmri --data-path "$TMPDIR"/data/singlecoil/ --exp-dir "$TMPDIR"/results/ --resolution 128 \
-#--recon-model-checkpoint /home/tbbakker/Projects/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
-#--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 100 \
-#--num-target-rows 16 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 1 --num-pools 4 --pool-stride 1 \
-#--estimator wr --acq_strat sample --acquisition None --center-volume True --num-test-trajectories 4 \
-#--wandb True --do-train-ssim True
+--of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 1000 \
+--num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.5 --num-epochs 50 --num-pools 4 --pool-stride 1 \
+--estimator wr --acq_strat sample --acquisition None --center-volume True --scheduler-type multistep --lr-multi-step-size 10 20 30 40 --project mrimpro_gamma_fix \
+--wandb True --do-train-ssim True --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
 
 #Copy output directory from scratch to home
-cp -r "$TMPDIR"/results $HOME/Projects/mrimpro/results
+cp -r "$TMPDIR"/results $HOME/Projects/mrimpro/gamma_results_fix
