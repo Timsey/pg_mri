68cf27776c7d8c55cce232e8e18625c4a3b1d900
Timsey <tim_bb@hotmail.com>
2020-09-11 15:34:30 +0200

    snr

--- local_runfiles/local_snr.sh
+++ local_runfiles/local_snr.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.calculate_snr -
 --data-path /home/timsey/HDD/data/fastMRI/singlecoil/ --recon-model-checkpoint /home/timsey/Projects/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
 --batch-size 16 --sample-rate 0.5 --seed 0 --num-workers 4 --num-trajectories 16 --acquisition None --center-volume True --batches-step 1 \
 --original_setting True --low_res False --style stoch --data_runs 3 --force_computation False --epochs 0 9 19 29 39 49 \
---impro_model_dir_list res128_al16_accel[8]_convpool_nounc_k8_2020-08-22_01:00:23_XXRJZ res128_al16_accel[8]_convpool_nounc_k8_2020-08-23_05:57:01_IIQRM \
-res128_al16_accel[8]_convpool_nounc_k8_2020-08-21_09:05:20_YFHWB res128_al16_accel[8]_convpool_nounc_k8_2020-08-22_12:56:28_FPWBU \
-res128_al16_accel[8]_convpool_nounc_k8_2020-08-20_17:52:34_EUNVB res128_al16_accel[8]_convpool_nounc_k8_2020-08-23_06:55:48_JJHOR \
---base_impro_model_dir /home/timsey/Projects/mrimpro/gamma_results/
+--impro_model_dir_list res128_al16_accel[8]_convpool_nounc_k8_2020-09-06_13:50:55_JAXUW res128_al16_accel[8]_convpool_nounc_k8_2020-09-07_04:22:39_WHLIG \
+res128_al16_accel[8]_convpool_nounc_k8_2020-09-06_00:35:18_LTKUH res128_al16_accel[8]_convpool_nounc_k8_2020-09-02_16:39:26_PTMDD \
+res128_al16_accel[8]_convpool_nounc_k8_2020-09-05_21:52:42_KOZOO res128_al16_accel[8]_convpool_nounc_k8_2020-09-04_15:02:47_EPPYS \
+res128_al16_accel[8]_convpool_nounc_k8_2020-09-07_03:07:37_GEEHR --base_impro_model_dir /home/timsey/Projects/mrimpro/gamma_results_fix/
