7e4e1c0ee40bfc2b69414f3da8df367d73abe95d
Timsey <tim_bb@hotmail.com>
2020-09-14 13:22:45 +0200

    das

--- das5_runfiles/das5_QR_gamma_fix.sh
+++ das5_runfiles/das5_QR_gamma_fix.sh
@@ -9,20 +9,11 @@ echo "Starting"
 source /var/scratch/tbbakker/anaconda3/bin/activate fastmri
 nvidia-smi
 
-# WOR baseline
-CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQR_model_sweep \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/gamma_results_fix/ --resolution 128 \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
---of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 1000 \
---num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wor --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim True --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
-
-# No baseline
+# WR baseline
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/singlecoil/ --exp-dir /var/scratch/tbbakker/mrimpro/gamma_results_fix/ --resolution 128 \
 --recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
 --estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim True --num-test-trajectories 1 --original_setting True --low_res False --no_baseline True
+--wandb True --do-train-ssim True --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
