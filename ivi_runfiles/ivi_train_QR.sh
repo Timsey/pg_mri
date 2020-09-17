8f3556f11174c185822baab0b121bde7c9a533ce
Timsey <tim_bb@hotmail.com>
2020-09-17 16:55:13 +0200

    ivi

--- ivi_runfiles/ivi_train_QR.sh
+++ ivi_runfiles/ivi_train_QR.sh
@@ -24,37 +24,37 @@ CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_m
 --recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline True
+--estimator wor --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
+--wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
 
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path /home/tbbakke/data/fastMRI/singlecoil/ --exp-dir /home/tbbakke/mrimpro/gamma_results_fix/ --resolution 128 \
 --recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline True
+--estimator wor --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
+--wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
 
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path /home/tbbakke/data/fastMRI/singlecoil/ --exp-dir /home/tbbakke/mrimpro/gamma_results_fix/ --resolution 128 \
 --recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline True
+--estimator wor --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
+--wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
 
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path /home/tbbakke/data/fastMRI/singlecoil/ --exp-dir /home/tbbakke/mrimpro/gamma_results_fix/ --resolution 128 \
 --recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline True
+--estimator wor --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
+--wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
 
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_model_sweep \
 --dataset fastmri --data-path /home/tbbakke/data/fastMRI/singlecoil/ --exp-dir /home/tbbakke/mrimpro/gamma_results_fix/ --resolution 128 \
 --recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.5 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
---estimator wr --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
---wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline True
+--estimator wor --acq_strat sample --acquisition None --center-volume True --lr-step-size 40 --project mrimpro_gamma_fix \
+--wandb True --do-train-ssim False --num-test-trajectories 1 --original_setting True --low_res False --no_baseline False
