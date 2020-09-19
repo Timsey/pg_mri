f577b0230ef168e1805f8ee899fd701c2dd94248
Timsey <tim_bb@hotmail.com>
2020-09-19 12:15:12 +0200

    lisa and das brain256 runfiles

--- das5_runfiles/das5_sbatch_brain_RL.sh
+++ das5_runfiles/das5_sbatch_brain_RL.sh
@@ -9,10 +9,10 @@ echo "Starting"
 source /var/scratch/tbbakker/anaconda3/bin/activate fastmri
 nvidia-smi
 
-# Cannot run batch 16 mid or batch 8 long with 192 res
+# Cannot run batch 16 mid or batch 8 long with 192 res, let alone 256: need to use batches-step >= 2
 CUDA_VISIBLE_DEVICES=0,1,2,3 HDF5_USE_FILE_LOCKING=FALSE PYTHONPATH=/var/scratch/tbbakker/anaconda3/envs/fastmri/lib/python3.7/site-packages python -m src.train_RL_model_sweep \
---dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/brain/ --exp-dir /var/scratch/tbbakker/mrimpro/brain_exp_results/ --resolution 256 \
---recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_brain_lowres192_8to4in2/model.pt --recon-model-name nounc \
+--dataset fastmri --data-path /var/scratch/tbbakker/data/fastMRI/brain/ --exp-dir /var/scratch/tbbakker/mrimpro/brain256_results/ --resolution 256 \
+--recon-model-checkpoint /var/scratch/tbbakker/fastMRI-shi/models/unet/al_brain_nonorig_highres256_8to4in2/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 16 --batch-size 8 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 1000 \
 --lr 5e-5 --sample-rate 0.2 --seed 0 --num-workers 4 --in-chans 1 --num-epochs 50 --num-pools 4 --pool-stride 1 \
 --estimator full_step --num-trajectories 8 --num-dev-trajectories 4 --greedy False --data-range volume --baseline-type selfstep \
