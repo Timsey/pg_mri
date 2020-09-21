49ed22bc97d606435957d21f93921e6d9f2fd5e2
Timsey <tim_bb@hotmail.com>
2020-09-21 13:56:16 +0200

    brain das5

--- das5_runfiles/das5_sbatch_brain_RL.sh
+++ das5_runfiles/das5_sbatch_brain_RL.sh
@@ -17,5 +17,5 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 HDF5_USE_FILE_LOCKING=FALSE PYTHONPATH=/var/scratch
 --of-which-four-pools 0 --num-chans 16 --batch-size 8 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 1000 \
 --lr 5e-5 --sample-rate 0.2 --seed 0 --num-workers 4 --in-chans 1 --num-epochs 50 --num-pools 5 --pool-stride 1 \
 --estimator full_step --num-trajectories 8 --num-dev-trajectories 4 --greedy False --data-range volume --baseline-type selfstep \
---scheduler-type multistep --lr-multi-step-size 10 20 30 40 --lr-gamma .5 --acquisition None --center-volume False --batches-step 4 \
+--scheduler-type multistep --lr-multi-step-size 10 20 30 40 --lr-gamma .5 --acquisition None --center-volume False --batches-step 2 \
 --wandb True --do-train-ssim False --project mrimpro_brain --original_setting False --low_res False --gamma 1.0
