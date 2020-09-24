b1fe400f1ae7e26e20de722fea5e60de08f40ae3
Timsey <tim_bb@hotmail.com>
2020-09-24 15:20:09 +0200

    ivi;

--- ivi_runfiles/ivi_train_brain_RL.sh
+++ ivi_runfiles/ivi_train_brain_RL.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:3
-#SBATCH --cpus-per-task=30
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=40
 #SBATCH --priority=TOP
 #SBATCH --mem=40G
 #SBATCH --verbose
@@ -20,7 +20,16 @@ nvidia-smi
 
 # Do your stuff
 
-CUDA_VISIBLE_DEVICES=0,1,2 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_RL_model_sweep \
+CUDA_VISIBLE_DEVICES=0,1,2,3 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_RL_model_sweep \
+--dataset fastmri --data-path /home/tbbakke/data/fastMRI/brain/ --exp-dir /home/tbbakke/mrimpro/brain_exp_results/ --resolution 256 \
+--recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_brain_nonorig_highres256_8to4in2/model.pt --recon-model-name nounc \
+--of-which-four-pools 0 --num-chans 8 --batch-size 4 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
+--lr 5e-5 --sample-rate 0.2 --seed 0 --num-workers 4 --in-chans 1 --num-epochs 50 --num-pools 5 --pool-stride 1 \
+--estimator full_step --num-trajectories 8 --num-dev-trajectories 4 --greedy False --data-range volume --baseline-type selfstep \
+--scheduler-type multistep --lr-multi-step-size 10 20 30 40 --lr-gamma .5 --acquisition None --center-volume False --batches-step 4 \
+--wandb True --do-train-ssim False --project mrimpro_brain --original_setting False --low_res False --gamma 1.0
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_RL_model_sweep \
 --dataset fastmri --data-path /home/tbbakke/data/fastMRI/brain/ --exp-dir /home/tbbakke/mrimpro/brain_exp_results/ --resolution 256 \
 --recon-model-checkpoint /home/tbbakke/fastMRI-shi/models/unet/al_brain_nonorig_highres256_8to4in2/model.pt --recon-model-name nounc \
 --of-which-four-pools 0 --num-chans 8 --batch-size 4 --impro-model-name convpool --fc-size 256 --accelerations 32 --acquisition-steps 28 --report-interval 1000 \
