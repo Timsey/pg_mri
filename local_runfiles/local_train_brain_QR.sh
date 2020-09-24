e1731d8f2e9db5a2f32bdb351747f641a0cc5983
Timsey <tim_bb@hotmail.com>
2020-09-24 12:30:36 +0200

    local

--- local_runfiles/local_train_brain_QR.sh
+++ local_runfiles/local_train_brain_QR.sh
@@ -8,4 +8,4 @@ CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.train_improQR_m
 --of-which-four-pools 0 --num-chans 8 --batch-size 16 --impro-model-name convpool --fc-size 256 --accelerations 8 --acquisition-steps 16 --report-interval 1000 \
 --num-target-rows 8 --lr 5e-5 --sample-rate 0.2 --seed 0 --num-workers 4 --in-chans 1 --lr-gamma 0.1 --num-epochs 50 --num-pools 5 --pool-stride 1 \
 --estimator wr --acq_strat sample --acquisition None --center-volume False --lr-step-size 40 --wandb True --do-train-ssim False --num-test-trajectories 1 \
---project mrimpro_brain --original_setting False --low_res True
+--project mrimpro_brain --original_setting False --low_res False
