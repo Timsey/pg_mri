3f62ec1450f62dcc6b62a2b60673027fa6c63178
Timsey <tim_bb@hotmail.com>
2020-10-16 11:37:36 +0200

    update bash

--- src/run_baseline.sh
+++ src/run_baseline.sh
@@ -3,7 +3,7 @@
 source /home/timsey/anaconda3/bin/activate rim
 
 CUDA_VISIBLE_DEVICES=0 HDF5_USE_FILE_LOCKING=FALSE python -m src.run_baseline_models \
---dataset brain --data_path /home/timsey/HDD/data/fastMRI/brain/ --exp_dir /home/timsey/Projects/mrimpro/refactor/ --resolution 256 \
---recon_model_checkpoint /home/timsey/Projects/fastMRI-shi/models/unet/al_brain_nonorig_highres256_8to4in2/model.pt \
---batch_size 2 --accelerations 32 --acquisition_steps 28 --sample_rate 0.2 \
---center_volume False --wandb True --project mrimpro_brain --partition test --model_type oracle
+--dataset knee --data_path /home/timsey/HDD/data/fastMRI/singlecoil/ --exp_dir /home/timsey/Projects/mrimpro/refactor/ --resolution 128 \
+--recon_model_checkpoint /home/timsey/Projects/fastMRI-shi/models/unet/al_nounc_res128_8to4in2_cvol_symk/model.pt \
+--batch_size 16 --accelerations 8 --acquisition_steps 16 --sample_rate 0.5 \
+--center_volume True --wandb True --project mrimpro_brain --partition test --model_type average_oracle
